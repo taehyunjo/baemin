7f23d06f94cd8f61bfca243296f160e10c0edcfa
unknown <comemania@naver.com>
2018-07-06 14:20:13 +0900

    db_version

--- core/db.py
+++ core/db.py
@@ -9,19 +9,18 @@ class db_machine():
         self.host = config.DATABASE_CONFIG['host']
         self.port = config.DATABASE_CONFIG['port']
         self.user = config.DATABASE_CONFIG['user']
-        self.pw = config.DATABASE_CONFIG['password'']
-        self.db_name = config.DATABASE_CONFIG['db_name'']
+        self.pw = config.DATABASE_CONFIG['password']
+        self.db_name = config.DATABASE_CONFIG['db_name']
 
         self.url = 'postgres://' + self.user + ":" + self.pw + "@" + self.host + ":" + self.port + "/" + self.db_name
 
         self.engine = create_engine(self.url)
-        self.metadata = MetaData(engine)
 
     def get_db_table_list(self):
-
+        pass
 
     def get_db_table(self,table_name):
-
+        pass
 
 
 
